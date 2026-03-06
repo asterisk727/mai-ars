82ab8f89c03aa72b0262042fa9d0dd190ded723d
asterisk727 <59166650+asterisk727@users.noreply.github.com>
2026-03-06 11:32:04 -0500

    feat: (wip) landing page

--- README.md
+++ README.md
@@ -15,7 +15,7 @@ To recreate this project with the same configuration:
 
 ```sh
 # recreate this project
-bun x sv@0.12.4 create --template minimal --types ts --add prettier eslint drizzle="database:sqlite+sqlite:libsql" better-auth="demo:password" --install bun mai-ars
+bun x sv@0.12.5 create --template minimal --types ts --add prettier eslint drizzle="database:sqlite+sqlite:libsql" better-auth="demo:password" --install bun mai-ars
 ```
 
 ## Developing
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,55 @@
 <script lang="ts">
-	import favicon from '$lib/assets/favicon.svg';
+	import { resolve } from '$app/paths';
+
+	import '../app.css';
 
 	let { children } = $props();
 </script>
 
-<svelte:head>
-	<link rel="icon" href={favicon} />
-</svelte:head>
+<div id="app">
+	<nav>
+		<a href={resolve('/')} class="logo text-gradient">
+			<span>MaiARS</span>
+		</a>
+		<div class="nav-links">
+			<a href={resolve('/ranking')}>
+				<span>rankings</span>
+			</a>
+			<a href={resolve('/user')}>
+				<span>account</span>
+			</a>
+		</div>
+	</nav>
 
-{@render children()}
+	{@render children()}
+</div>
+
+<style>
+	#app {
+		background: var(--background-primary);
+		min-height: 100vh;
+	}
+
+	nav {
+		display: flex;
+		position: sticky;
+		justify-content: space-between;
+		align-items: center;
+
+		top: 0;
+		height: 2rem;
+		z-index: 100;
+		padding: 1rem 2.5rem;
+	}
+
+	.logo {
+		font-size: 1.5rem;
+		font-weight: 500;
+	}
+
+	.nav-links {
+		display: flex;
+		justify-content: flex-end;
+		gap: 25px;
+	}
+</style>
